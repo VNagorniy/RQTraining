c6324dd6805ec7b3725a01604b205a025e329aaf
VNagorniy <vnagorniyrnd@gmail.com>
2024-02-09 22:13:58 +0300

    проработка структуры приложения

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Fragment, Component } from 'react';
+import Link from './Link.js';
+
+class App extends Component {
+  render() {
+    const link1 = React.createElement(Link, {
+      framework: 'React',
+      url: '//react.dev',
+    });
+    const link2 = React.createElement(Link, {
+      framework: 'Vue',
+      url: '//vuejs.org',
+    });
+    const link3 = React.createElement(Link, {
+      framework: 'Angular',
+      url: '//angular.io',
+    });
+    return React.createElement(Fragment, null, link1, link2, link3);
+  }
+}
+export default App;
