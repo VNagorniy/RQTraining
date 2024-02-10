bd025b5149e4cbc368bb1bbd8ff9d7180c96a6fd
VNagorniy <vnagorniyrnd@gmail.com>
2024-02-10 19:03:31 +0300

    разбор JSX (создание, работа с props, ветвления, фрагменты)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Fragment, Component } from 'react';
+// import ProfileLink from './Link.js';
+// import ButtonList from './Link.js';
 import Link from './Link.js';
 
 class App extends Component {
   render() {
-    const link1 = React.createElement(Link, {
-      framework: 'React',
-      url: '//react.dev',
-    });
-    const link2 = React.createElement(Link, {
-      framework: 'Vue',
-      url: '//vuejs.org',
-    });
-    const link3 = React.createElement(Link, {
-      framework: 'Angular',
-      url: '//angular.io',
-    });
-    return React.createElement(Fragment, null, link1, link2, link3);
+    return (
+      <Fragment>
+        <Link url='//react.dev'>
+          <strong>React</strong>
+        </Link>
+        <Link url='//vuejs.org'>Vue</Link>
+        <Link url='//angular.io'>Angular</Link>
+      </Fragment>
+    );
   }
 }
 export default App;
